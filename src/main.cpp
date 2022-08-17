c38bba2873dbed3141b2df14d3be538009d137d6
Oleg Nesterenkov <olegnest20@gmail.com>
2022-08-17 23:23:08 +0300

    Refactor + new webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,83 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+#include <WebServer.h>
 #include <WiFi.h>
 
-/***
- * LED Blinking
- * Code Written by TechMartian
- */
 int leds [] = { 16, 22, 19 }; // R-G-B
-// int led = 0;
 
 int brightness = 0;
 int brightStep = 1;
 
-// the number of the LED pin
 const int ledPin = 16;  // 16 corresponds to GPIO16
 
-WiFiServer server(80);
+WebServer server(80);
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("KONKITAYSKIY", "sahdia7shf7ase83");
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
+String getMIME(const String& filename)
+{
+  if (filename.endsWith(".html"))
+    return "text/html";
+  else if (filename.endsWith(".css"))
+    return "text/css";
+  else if (filename.endsWith(".js"))
+    return "application/javascript";
+  else if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  else if (filename.endsWith(".jpg"))
+    return "image/jpeg";
+  else if (filename.endsWith(".png"))
+    return "image/png";
+  return "text/plain";
 }
 
-void setLED(char mark, int level){
+bool handlePage(const String& filePath) {
+  String contentType = getMIME(filePath);
+  if (SPIFFS.exists(filePath)) {
+    File file = SPIFFS.open(filePath, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  else {
+    String err = "File " + filePath + " not found!";
+    Serial.println(err);
+    return false;
+  }
+}
+
+void initWiFi(WebServer& server) {
+  File configFile = SPIFFS.open("/config/wifi.json", "r");
+  const char *ssid = "", *pass = "";
+  if (configFile) {
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, configFile);
+    if (error) {
+      Serial.print("Failed to read a config file: ");
+      Serial.println(error.c_str());
+    } else {
+      ssid = doc["ssid"];
+      pass = doc["password"];
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, pass);
+      unsigned long startTime = millis();
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        if ((unsigned long)(millis() - startTime) >= 7000) {
+          Serial.println("Error while try to connect to WI-FI!");
+        }
+      }
+    }
+  } else {
+    Serial.println("Config file was not found!");
+  }
+  Serial.println("Starting the webserver on " + WiFi.localIP());
+  server.begin();
+}
+
+void setLED(char mark, int level) {
   int selectedPin = 0;
-  switch (mark)
-  {
-  // case 'R':
-  //   selectedPin = leds[0];
-  //   break;
+  switch (mark) {
   case 'G':
     selectedPin = leds[1];
     break;
@@ -46,35 +90,18 @@ void setLED(char mark, int level){
   analogWrite(selectedPin, level);   
 }
 
-// void setup() {
-//   Serial.begin(115200);
-//   initWiFi();
-//   // pinMode (leds[0], OUTPUT);
-//   // pinMode (leds[1], OUTPUT);
-//   // pinMode (leds[2], OUTPUT);
-// }
-// void loop() {
-//   // if (led > 2)
-//   // {
-//   //   led = 0;
-//   // }
-
-//   // int cur_led = leds[led];
-  
-//   // digitalWrite (cur_led, HIGH);	// turn on the LED
-//   // delay(500);	// wait for half a second or 500 milliseconds
-//   // digitalWrite (cur_led, LOW);	// turn off the LED
-//   // delay(500);	// wait for half a second or 500 milliseconds
-
-//   // led++;
-
-  
-// }
-void setup(){
+void setup() {
   Serial.begin(115200);
+
+  SPIFFS.begin();
+  initWiFi(server);
+
+  server.on("/", []() {
+      handlePage("/templates/mainpage.html");
+    });
 }
  
-void loop(){
+void loop() {
   // increase the LED brightness
   brightness += brightStep;
 
